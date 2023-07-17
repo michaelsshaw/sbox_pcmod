93b1ae2091b2783583c9bcc12f10f681131acb4e
Sam54123 <thesam54123@gmail.com>
2023-07-16 20:39:39 -0700

    worked on game integration

---
+++ code/game/AppleEntity.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcmod.GameIntegration;
+
+public partial class AppleEntity : BasePhysics
+{
+    public override void Spawn()
+    {
+        base.Spawn();
+        SetModel("models/computer/apple2.vmdl");
+        Tags.Add("solid");
+    }
+}
