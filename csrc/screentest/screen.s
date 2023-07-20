f9ff2577233e11d6652b23da59832a0cda39f5db
Michael Shaw <spencershawiscool@gmail.com>
2023-07-19 22:04:54 -0400

    csrc: add initial asm file and linker script

---
+++ csrc/screentest/screen.s
@@ -0,0 +1,20 @@
+; SPDX-License-Identifier: GPL-2.0-only
+
+ap_mon = $C000
+
+.code
+_main:
+	lda #$42
+	ldx #$69
+	ldy #$AB
+
+_nmi:
+	rti
+_irq:
+	rti
+
+.segment "VECTORS"
+.org $FFFA
+.addr _nmi
+.addr _main
+.addr _irq
