6dd1f0fa57bc12489076736f8d91f20ee0129388
Michael Shaw <spencershawiscool@gmail.com>
2023-07-22 14:45:48 -0400

    screentest.s: create screentest function in ASM

--- csrc/screentest/screen.s
+++ csrc/screentest/screen.s
@@ -1,12 +1,52 @@
 ; SPDX-License-Identifier: GPL-2.0-only
 
-ap_mon = $C000
+ap_mon_dma = $C000
+pwof_addr = $C001
+
+dma_begin = $2900
+dma_begin_lo = $00
+dma_begin_hi = $29
+
+dma_store_lo = $40
+dma_store_hi = $41
 
 .code
 _main:
-	lda #$42
-	ldx #$69
-	ldy #$AB
+	; init registers
+	lda #0
+	ldx #0
+	ldy #0
+	
+	; init begin dma address
+	pha
+	lda #dma_begin_lo
+	sta dma_store_lo
+	lda #dma_begin_hi
+	sta dma_store_hi
+	pla
+
+	; store 256 bytes into memory then increment the stored dma address
+	ldx #$FF
+
+_begin_loop:
+	inx
+	sta (dma_store_lo),y
+	iny
+	beq _inc_dma
+
+_inc_dma:
+	inc dma_store_hi
+	beq _done
+	jmp _begin_loop
+
+_done:
+	; initiate dma and end program
+	lda #00
+	sta ap_mon_dma
+	sta pwof_addr
+	
+	; should never happen
+	jmp _hlt
 
 _hlt:
 	jmp _hlt
