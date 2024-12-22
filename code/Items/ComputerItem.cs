4339c7ae504b43d61b7a3bc8c4e0928677cab4d8
Sam54123 <thesam54123@gmail.com>
2024-12-21 16:02:35 -0800

    fucking around with scene-component system

---
+++ code/Items/ComputerItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public sealed class ComputerItem : Component, Component.IPressable
+{
+	public bool Press( IPressable.Event e )
+	{
+		throw new NotImplementedException();
+	}
+}
